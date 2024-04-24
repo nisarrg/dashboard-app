fc99f678d441c2184cb50a2e822da545d475066c
nisarrg <nisargk007@gmail.com>
2024-04-24 02:14:38 -0300

    added 3 row components

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,8 +1,9 @@
 import { Box, useMediaQuery } from '@mui/material';
-import { useTheme } from '@emotion/react';
 import DashboardBox from '@/components/DashBoardBox';
+import Row1 from './Row1';
+import Row2 from './Row2';
+import Row3 from './Row3';
 
-type Props = {}
 const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
@@ -49,9 +50,8 @@ const gridTemplateSmallScreens = `
   "j"
 `;
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1200px")
-    const  { palette } = useTheme();
   return (
     <Box width="100%" height="100%" display="grid" gap="1.5rem"
         sx={
@@ -67,16 +67,9 @@ const Dashboard = (props: Props) => {
     }    
     >
         <DashboardBox />
-        <DashboardBox  gridArea="b"></DashboardBox>
-        <DashboardBox  gridArea="c"></DashboardBox>
-        <DashboardBox  gridArea="d"></DashboardBox>
-        <DashboardBox  gridArea="a"></DashboardBox>
-        <DashboardBox  gridArea="e"></DashboardBox>
-        <DashboardBox  gridArea="f"></DashboardBox>
-        <DashboardBox  gridArea="g"></DashboardBox>
-        <DashboardBox  gridArea="h"></DashboardBox>
-        <DashboardBox  gridArea="i"></DashboardBox>
-        <DashboardBox  gridArea="j"></DashboardBox>
+       <Row1 />
+       <Row2 />
+       <Row3 />
     </Box>
   )
 }
