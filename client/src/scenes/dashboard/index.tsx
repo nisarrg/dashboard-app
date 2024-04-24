f0826db04f4910c0eda1757f56c6302242a9b702
nisarrg <nisargk007@gmail.com>
2024-04-24 01:52:09 -0300

    added styles to grid boxes

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,8 +1,9 @@
-import { Box } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
 import { useTheme } from '@emotion/react';
+import DashboardBox from '@/components/DashBoardBox';
 
 type Props = {}
-const gridTemplate = `
+const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
     "a b c"
@@ -13,28 +14,69 @@ const gridTemplate = `
     "g h i"
     "g h j"
     "g h j"
-`
+`;
+
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+`;
 
 const Dashboard = (props: Props) => {
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1200px")
     const  { palette } = useTheme();
   return (
     <Box width="100%" height="100%" display="grid" gap="1.5rem"
-        sx={{
+        sx={
+            isAboveMediumScreens? {
             gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
             gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-            gridTemplateAreas: gridTemplate,
-        }}    
+            gridTemplateAreas: gridTemplateLargeScreens,
+        } : {
+            gridAutoColumns: "1fr",
+            gridAutoRows: "80px",
+            gridTemplateAreas: gridTemplateSmallScreens,            
+        }
+    }    
     >
-        <Box bgcolor="#fff" gridArea="a"></Box>
-        <Box bgcolor="#fff" gridArea="b"></Box>
-        <Box bgcolor="#fff" gridArea="c"></Box>
-        <Box bgcolor="#fff" gridArea="d"></Box>
-        <Box bgcolor="#fff" gridArea="e"></Box>
-        <Box bgcolor="#fff" gridArea="f"></Box>
-        <Box bgcolor="#fff" gridArea="g"></Box>
-        <Box bgcolor="#fff" gridArea="h"></Box>
-        <Box bgcolor="#fff" gridArea="i"></Box>
-        <Box bgcolor="#fff" gridArea="j"></Box>
+        <DashboardBox />
+        <DashboardBox  gridArea="b"></DashboardBox>
+        <DashboardBox  gridArea="c"></DashboardBox>
+        <DashboardBox  gridArea="d"></DashboardBox>
+        <DashboardBox  gridArea="a"></DashboardBox>
+        <DashboardBox  gridArea="e"></DashboardBox>
+        <DashboardBox  gridArea="f"></DashboardBox>
+        <DashboardBox  gridArea="g"></DashboardBox>
+        <DashboardBox  gridArea="h"></DashboardBox>
+        <DashboardBox  gridArea="i"></DashboardBox>
+        <DashboardBox  gridArea="j"></DashboardBox>
     </Box>
   )
 }
