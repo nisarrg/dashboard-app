d067f12931dac8a8e29f6fe48ca4ee1015acc658
nisarrg <nisargk007@gmail.com>
2024-04-25 00:35:21 -0300

    row3 done

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import { kpis, products } from "./data/data.js";
+import { kpis, products, transactions } from "./data/data.js";
 import KPI from "./models/KPI.js";
 import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js";
@@ -44,5 +44,6 @@ mongoose
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
     // Product.insertMany(products)
+    // Transaction.insertMany(transactions)
   })
   .catch((error) => console.log(`${error} did not connect`));
